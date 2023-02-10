988d2ef63677a517aace39e0fab7fcf8ba497bdc
acerelite <61611934+acerelite@users.noreply.github.com>
2023-02-10 18:37:11 +0800

    Add design and structure

---
+++ script.js
@@ -0,0 +1,8 @@
+let pwd = document.querySelector("#password");
+let c_pwd = document.querySelector("#c_password");
+
+
+pwd.addEventListener("change", checkPass);
+c_password.addEventListener("change", checkPass);
+
+
